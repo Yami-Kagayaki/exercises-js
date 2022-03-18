8454e38a181911e70e1113bc16631bbad9820135
Yami-Kagayaki <lucasprogjr@gmail.com>
2022-03-18 03:59:15 -0300

    Atualizando

--- Chute até acertar automático/index.js
+++ Chute até acertar automático/index.js
@@ -3,14 +3,14 @@ let num = Math.floor(Math.random() * maxChute);
 let joker = Math.floor(Math.random() * maxChute);
 let max = maxChute;
 let min = 0;
-let tentativas = 0;
+let tentativas = 1;
 
 console.log("Jogo Iniciado, num máximo é:", maxChute);
 
 while (num != joker) {
   if (num > joker) {
     console.log("digitado:", num);
-    max = num;
+    max = ++num;
     console.log("Perdeu, é menor, tente dnv");
     num = Math.floor(Math.random() * (max - min) + min);
     tentativas++;
@@ -18,7 +18,7 @@ while (num != joker) {
   }
   if (num < joker) {
     console.log("digitado:", num);
-    min = num;
+    min = ++num;
     console.log("Perdeu, é maior, tente dnv");
     num = Math.floor(Math.random() * (max - min) + min);
     tentativas++;
@@ -26,6 +26,7 @@ while (num != joker) {
   }
 }
 
+console.log("digitado:", num);
 console.log("Vitoria");
 console.log("o numero era:", joker);
 console.log("total de tentativas:", tentativas);
